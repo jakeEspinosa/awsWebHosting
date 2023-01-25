429c49e61a81be18f512e7e6c676527dce2b8c6e
Jake Espinosa <50299748+jakeEspinosa@users.noreply.github.com>
2023-01-24 20:29:08 -0700

    Add files via upload

---
+++ etchASketch/js/script.js
@@ -0,0 +1,72 @@
+const gridDiv = document.getElementById('grid-div');
+
+function createRows(rows){
+  for (let i = 1; i < rows + 1; i++) {
+    const newRow = document.createElement('div');
+    newRow.className = 'row';
+    gridDiv.appendChild(newRow);
+  }
+}
+
+function populateRows(squares) {
+  const rowList = gridDiv.querySelectorAll('.row');
+
+  for (const child of rowList) {
+    for (i = 1; i < squares + 1; i++) {
+      const newBox = document.createElement('div');
+      newBox.className = 'square';
+      child.appendChild(newBox);
+    }
+  }
+}
+
+function etchASketch() {
+  const boxes = document.querySelectorAll('.square');
+
+  boxes.forEach(box => {
+    box.addEventListener('mouseover', (hoveredBox) => {
+      hoveredBox.currentTarget.classList.add('hoveredOver');
+    });
+  });
+}
+
+function initializeGrid(size) {
+  createRows(size);
+  populateRows(size);
+  etchASketch();
+}
+
+function clearGrid() {
+  while (gridDiv.firstChild) {
+    gridDiv.removeChild(gridDiv.firstChild);
+  }
+}
+
+function resizeGrid() {
+  while (true) {
+    let gridSize = parseInt(prompt('What size would you like the grid to be?'));
+    if (gridSize <= 75) {
+      clearGrid();
+      initializeGrid(gridSize);
+      break;
+    } else {
+      alert('Please enter a number less than or equal to 75!');
+    }
+  }
+}
+
+function resetGrid() {
+  const boxes = document.querySelectorAll('.square');
+
+  boxes.forEach(box => {
+    box.classList.remove('hoveredOver')
+  });
+}
+
+initializeGrid(16);
+
+const resizeButton = document.querySelector('#resize');
+resizeButton.addEventListener('click', resizeGrid);
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', resetGrid)
